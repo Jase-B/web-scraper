d60ef7eb0fdd9e88ac40894906d403aa34c42e66
Jase Balderrama <jasebalderrama@icloud.com>
2019-06-01 12:13:28 -0700

    Add logging to routeTo()

--- src/WebPage/__proto__/_routeTo.js
+++ src/WebPage/__proto__/_routeTo.js
@@ -1,8 +1,11 @@
 const routeTo = async function (path) {
+  console.log('ROUTING\n');
+
   return Promise.all([
     this.page.waitForNavigation({ waitUntil: 'networkidle0' }),
     this.page.goto(path)
-  ]);
+  ])
+  .then(() => console.log(`ROUTED_TO: ${path}\n`));
 };
 
 module.exports = routeTo;
