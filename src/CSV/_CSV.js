ba6b409e6088b39497650430fdfb5cf2ee1fb2f3
Jase Balderrama <jasebalderrama@icloud.com>
2019-06-11 13:38:05 -0700

    Add starter code to _CSV.js

--- src/CSV/_CSV.js
+++ src/CSV/_CSV.js
@@ -1,21 +1,34 @@
 const fs = require('fs');
+const { google } = require('googleapis');
 const fastCsv = require('fast-csv');
-const protos = require('./__proto__');
+// const protos = require('./__proto__');
 
-function CSV (opts) {
-  const {
-    headers,
-    quoteHeaders,
-    outputPath,
-  } = opts;
+function CSV () {
+  // const {
+  //   headers,
+  //   quoteHeaders,
+  //   outputPath,
+  // } = opts;
 
-  this.targetStream = fastCsv.createWriteStream(headers, quoteHeaders);
-  this.destStream = fs.createWriteStream(outputPath, { flags: 'as' });
+  // this.targetStream = fastCsv.createWriteStream(headers, quoteHeaders);
+  // this.destStream = fs.createWriteStream(outputPath, { flags: 'as' });
 
-  this.targetStream.pipe(this.destStream);
+  // this.targetStream.pipe(this.destStream);
+
+  // const creds = fs.readFileSync('./google-credentials.json', 'utf8');
+  // const {
+  //   installed: {
+  //     client_id, client_secret, redirect_uris
+  //   }
+  // } = JSON.parse(creds);
+  // const oAuth = new google.auth.OAuth2(
+  //   client_id, client_secret, redirect_uris[0]
+  // );
+
+  // oAuth.getToken()
+  // console.log(oAuth)
 }
 
-CSV.prototype = protos;
-
+// CSV.prototype = protos;
 
 module.exports = CSV;
